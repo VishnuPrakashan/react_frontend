b57cbb84be77a480d379cad04b94bbf34323be1f
VishnuPrakashan <vishnuprakashan2022@gmail.com>
2023-06-16 12:53:18 +0530

    second-commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,16 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./components/login/Login";
 import Signup from "./components/signup/Signup";
 import Home from "./components/home/Home";
+import axios from "axios";
+import { Toaster } from "react-hot-toast";
+
+axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
     <>
+      <Toaster position="top-center" toastOptions={{ duration: 2000 }} />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,38 +1,67 @@
-import React from 'react'
-import './Login.css'
-import {AiOutlineEye} from "react-icons/ai";
+import React, { useState } from "react";
+import "./Login.css";
+import { AiOutlineEye } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const loginUser = (e) => {
+    e.preventDefault();
+
+    axios.get('/')
+  };
+
   return (
     <div>
-        <div className="nav">
+      <div className="nav">
         <span className="text">Don't have an account?</span>
-        <Link to={"/signup"}><button typeof="submit" className="btn2">Sign Up</button></Link>
-        </div>
+        <Link to={"/signup"}>
+          <button typeof="submit" className="btn2">
+            Sign Up
+          </button>
+        </Link>
+      </div>
       <div className="container">
-        <form action="/" id="form">
+        <form id="form" onSubmit={loginUser}>
           <h1>Welcome back!</h1>
 
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" />
+          <input
+            type="email"
+            id="email"
+            value={data.email}
+            onChange={(e) => setData({ ...data, email: e.target.value })}
+          />
 
           <label htmlFor="pswd">Password</label>
-          <input type="password" id="pswd" />
-          <AiOutlineEye className="icon-pswd"/>
+          <input
+            type="password"
+            id="pswd"
+            value={data.password}
+            onChange={(e) => setData({ ...data, password: e.target.value })}
+          />
+          <AiOutlineEye className="icon-pswd" />
 
-          <div className='checkbox'>
-              <input type="checkbox" name="checkbox" id="checkbox"/>
-              <label htmlFor="checkbox">Remember me</label>
+          <div className="checkbox">
+            <input type="checkbox" name="checkbox" id="checkbox" />
+            <label htmlFor="checkbox">Remember me</label>
           </div>
-          <button type="submit" className="btn3">Login</button>
+          <button type="submit" className="btn3">
+            Login
+          </button>
         </form>
-        
       </div>
-        <div className='forgot'><a href="">Forgot your password?</a></div>
-        <div className="circle"></div>
+      <div className="forgot">
+        <a href="">Forgot your password?</a>
+      </div>
+      <div className="circle"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
